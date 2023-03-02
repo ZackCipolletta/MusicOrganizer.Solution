74f90db13de46ad70c070a7b2808bf6ce7b719b2
Mike <ZackCipolletta@gmail.com>
2023-03-02 09:15:38 -0800

    Add initial boilerplate code to each files
    Co-authored-by:  James Provance <jb24x7@gmail.com>

---
+++ MusicOrganizer.Tests/ModelTests/AlbumsTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Albums.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Albums.Tests
+{
+  [TestClass]
+  public class AlbumsTests : IDisposable
+  {
+
+  }
+}
---
+++ MusicOrganizer.Tests/ModelTests/ArtistsTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Artists.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Artists.Tests
+{
+  [TestClass]
+  public class ArtistsTests : IDisposable
+  {
+
+  }
+}
